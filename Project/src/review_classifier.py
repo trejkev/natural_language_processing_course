7a29a3ee6449cfc75b7b7f3a463663e358a90f24
Kevin Trejos Vargas <trejkev@gmail.com>
2023-10-20 09:10:44 -0600

    Removing unnecessary library

--- Project/src/review_classifier.py
+++ Project/src/review_classifier.py
@@ -36,7 +36,6 @@ from sklearn.metrics                 import accuracy_score
 from sklearn.metrics                 import precision_score
 from sklearn.metrics                 import recall_score
 from sklearn.metrics                 import f1_score
-from sklearn.metrics                 import ConfusionMatrixDisplay
 from sklearn.metrics                 import confusion_matrix
 
 import matplotlib.pyplot as plt
