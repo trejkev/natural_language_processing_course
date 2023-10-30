e81d12fd6f57e3fc386484b831f790cf52906e4f
Kevin Trejos Vargas <trejkev@gmail.com>
2023-10-29 22:28:32 -0600

    Minimum format changes

--- Project/src/review_classifier.py
+++ Project/src/review_classifier.py
@@ -91,15 +91,15 @@ y_pred = classifier.predict(X_test)
 ################################################################################
 # --                           Evaluate the model                           -- #
 ################################################################################
-accuracy       = accuracy_score(y_test, y_pred)
-precision      = precision_score(y_test, y_pred)
-recall         = recall_score(y_test, y_pred)
-f1             = f1_score(y_test, y_pred)
-confMatrix     = confusion_matrix(y_test, y_pred)
-tn             = confMatrix[0][0]
-fp             = confMatrix[0][1]
-tp             = confMatrix[1][1]
-fn             = confMatrix[1][0]
+accuracy   = accuracy_score(y_test, y_pred)
+precision  = precision_score(y_test, y_pred)
+recall     = recall_score(y_test, y_pred)
+f1         = f1_score(y_test, y_pred)
+confMatrix = confusion_matrix(y_test, y_pred)
+tn         = confMatrix[0][0]
+fp         = confMatrix[0][1]
+tp         = confMatrix[1][1]
+fn         = confMatrix[1][0]
 
 
 ################################################################################
@@ -142,8 +142,8 @@ sns.heatmap(
 )
 
 # -- Assign the plot labels
-plt.xlabel('Actual'                 )
-plt.ylabel('Predicted'              )
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
 plt.title('Confusion Matrix Heatmap')
 
 # -- Move the X-axis to the top
